a8ecce78e252ba22c3499c1cc0bee99703f728cf
zijunj <123190820+zijunj@users.noreply.github.com>
2026-03-09 02:25:49 -0400

    fixed router for github pages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/FESReact">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
